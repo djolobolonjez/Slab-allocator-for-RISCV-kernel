fdb6998b2afb94318903a6d4b79a284c3c3e14e9
djolobolonjez <djordjem@outlook.com>
2022-12-17 02:17:58 +0100

    typo

--- h/mmu.h
+++ h/mmu.h
@@ -6,25 +6,31 @@
 static const uint8 PAGE_ORDER = 12;
 static const uint64 PAGE_SIZE = 1 << 12;
 
-uint64* alignAddress(uint64* addr, uint64 order);
+class MMU {
+private:
+    uint64* alignAddress(uint64* addr, uint64 order);
 
-enum EntryBits {
-    Valid = 1 << 0,
-    Read = 1 << 1,
-    Write = 1 << 2,
-    Execute = 1 << 3,
-    User = 1 << 4,
-    Global = 1 << 5,
-    Access = 1 << 6,
-    Dirty = 1 << 7,
+    enum EntryBits {
+        Valid = 1 << 0,
+        Read = 1 << 1,
+        Write = 1 << 2,
+        Execute = 1 << 3,
+        User = 1 << 4,
+        Global = 1 << 5,
+        Access = 1 << 6,
+        Dirty = 1 << 7,
 
-    ReadWrite = Read | Write,
-    ReadExecute = Read | Execute,
-    ReadWriteExecute = Read | Write | Execute,
+        ReadWrite = Read | Write,
+        ReadExecute = Read | Execute,
+        ReadWriteExecute = Read | Write | Execute,
 
-    UserReadWrite = Read | Write | User,
-    UserReadExecute = Read | Execute | User,
-    UserReadWriteExecute = Read | Write | Execute | User,
+        UserReadWrite = Read | Write | User,
+        UserReadExecute = Read | Execute | User,
+        UserReadWriteExecute = Read | Write | Execute | User,
+    };
 };
+
+
+
 
 #endif // _MMU_H
--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,6 +1,6 @@
 #include "../h/mmu.h"
 
-uint64* alignAddress(uint64* addr, uint64 order) {
+uint64* MMU::alignAddress(uint64* addr, uint64 order) {
 
     uint64 mask = (1 << order) - 1;
     uint64 a = (uint64)addr;
