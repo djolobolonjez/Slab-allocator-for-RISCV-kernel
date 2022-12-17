635941a308eae9dd270cd405e5c3207b65070573
djolobolonjez <djordjem@outlook.com>
2022-12-17 13:52:48 +0100

    small fix

--- src/buddy.cpp
+++ src/buddy.cpp
@@ -18,6 +18,7 @@ size_t Buddy::greaterPowerOfTwo(size_t x, size_t* order) {
 void Buddy::buddyInit() {
     KERNEL_START_ADDR = (void*)HEAP_START_ADDR;
     size_t size = ((char*)HEAP_END_ADDR - (char*)HEAP_START_ADDR) * 125 / 1000;
+	
     size_t numOfBlocks = size / BLOCK_SIZE;
     size_t powerOfTwoBlocks = greaterPowerOfTwo(numOfBlocks, &MAX_ORDER);
 
@@ -28,7 +29,7 @@ void Buddy::buddyInit() {
 
     HEAP_START_ADDR = (char*)KERNEL_END_ADDR + 1;
 
-    blocks = (FreeArea**)KERNEL_START_ADDR;
-    KERNEL_START_ADDR = (char*)KERNEL_START_ADDR + MAX_ORDER * sizeof (FreeArea*);
-    blocks[MAX_ORDER - 1] = (FreeArea*) KERNEL_START_ADDR;
+    blocks = (FreeArea**) KERNEL_START_ADDR;
+    KERNEL_START_ADDR = (char*)KERNEL_START_ADDR + (MAX_ORDER + 1) * sizeof (FreeArea*);
+    blocks[MAX_ORDER] = (FreeArea*) KERNEL_START_ADDR;
 }
